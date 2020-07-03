7e839577b7acefb6a8034f119d01d6ccb6c67170
Vraj_codes <vinayakrajbusiness@gmail.com>
2020-07-03 13:47:16 +0530

    Update news.php

--- news.php
+++ news.php
@@ -3,61 +3,21 @@
   <title>news updates</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php  include 'newsstyle.php'; ?>
-  <?php  include 'link/links.php'; ?>
+  <link rel="stylesheet" type="text/css" href="newsstyle.css">
+  <?php  include 'links.php'; ?>
    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@500;600;700&display=swap" rel="stylesheet">
 
     
             
 </head>
 <body>
-  <nav class="navbar fixed-top navbar-expand-lg p-1 nav_style  navbar-dark primary-nav" id="primary-nav">
-  <a class="navbar-brand logo" href="#"><h1><span>NEWS</span>updates</h1></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
 
-    <div class="collapse navbar-collapse " id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto pr-5 text-capitalize p-3">
-        <!-- <li class="nav-item active">
-          <a class="nav-link" href="#">news updates <span class="sr-only">(current)</span></a>
-        </li> -->
-        <li class="nav-item active">
-          <a class="nav-link" href="index.php">Home </a>
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="#about">about </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#symptoms">symptoms </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#prevention">prevention </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#contact">contact </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="newsapi.php">News </a>
-        </li> -->
-      <!-- <button type="button" class="btn btn-warning p-0">
-        <li class="nav-item ">
-          <a class="nav-link" style="color: black;" href="tracker.php">Tracker</a>
-        </li>
-      </button> -->
-      </ul>
-    </div>
-  </nav>
-<br>
-<br>
-<br>
-<br>
   
 <div class="container">
     <div class="row">
       <div class="col text-center">
           <h1>
-      <img src="images/Capture.jpg" width="200" height="100">
+      <img src="Capture.jpg" width="200" height="100">
         </h1>
       </div>
   </div>
